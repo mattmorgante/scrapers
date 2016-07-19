dba86171fbcec5c28c7f4e2cd10c3c2394510658
Matt <matthewmorgante@gmail.com>
2016-07-19 16:00:50 +0200

    patagonia scrapers finished

--- tripadvisor.rb
+++ tripadvisor.rb
@@ -3,7 +3,7 @@ require 'Nokogiri'
 require 'JSON'
 require 'csv'
 
-page = HTTParty.get('https://www.tripadvisor.com/Attractions-g316035-Activities-El_Chalten_Province_of_Santa_Cruz_Patagonia.html')
+page = HTTParty.get('https://www.tripadvisor.com/Attractions-g312848-Activities-San_Carlos_de_Bariloche_Province_of_Rio_Negro_Patagonia.html')
 parse_page = Nokogiri::HTML(page)
 alt_tags = []
 names_array = []
@@ -17,3 +17,6 @@ my_hash = {}
 my_hash = Hash[names_array.zip(alt_tags)] 
 puts my_hash
 
+# hash = {"\nReserva Nacional Cerro Castillo\n"=>"5 of 5 stars", "\nCoyhaique National Reserve\n"=>"4.5 of 5 stars"} etc etc 
+
+# 
